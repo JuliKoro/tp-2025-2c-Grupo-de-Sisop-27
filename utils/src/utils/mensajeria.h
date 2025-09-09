3f3619909a0432b73695136712497945cce6a7f2
MercedesdelaTorre <mdelatorre@frba.utn.edu.ar>
2025-09-09 23:08:46 +0000

    msjs.h

--- utils/src/utils/mensajeria.h
+++ utils/src/utils/mensajeria.h
@@ -5,6 +5,33 @@
 #include <sys/socket.h>
 
 
+/**
+* @brief Obtiene informacion de la ip, crea un socket, lo conecta con el 
+* servidor
+* @param ip la ip del server
+* @param puerto puerto donde escucha el servidor
+* @return Devuelve el descriptor del socket conectado, o valor negativo
+* si hay error.
+*/
+int crear_conexion(const char* ip, const char* puerto);
+
+/**
+* @brief Recibe el socket del servidor y el logger del modulo
+* @param socket_servidor el socket del servidor que va a aceptar conexiones
+* @param logger el logger del modulo
+* @return devuelve un socket conectado al socket del servidor
+*/
+int esperar_cliente(int socket_servidor);
+
+/**
+* @brief Envia un string al socket que se pasa por parametro
+* @param mensaje el mensaje a enviar
+* @param socket el socket de destino
+* @param logger el logger del modulo que envia el mensaje
+* @return Nada 
+*/
+
+
 
 #endif
 
